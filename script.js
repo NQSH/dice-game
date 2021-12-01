0882d37a848de0e114224875bd7645f3e2e9e621
NQSH <thom.lecorre@gmail.com>
2021-12-01 18:44:03 +0100

    Update script.js
    + Ajout des fonctionnalités de changement d'interface

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const header = $('header');
-const menuWrapper = $('.menu-wrapper');
+const menuWrapper = $('#menu-wrapper');
+const gameScreen = $('#game-screen');
 
 const loadMenu = () => {
+
+  // LOADING ANIMATION
   setTimeout(() => {
     $('.menu-btn').first().click();
   }, 3000)
+
   header
   .delay(1500)
   .hide(1500);
+
   menuWrapper
   .delay(1500)
   .animate({
@@ -19,11 +24,37 @@ const loadMenu = () => {
       'opacity': '100%'
     }, 1000, () => {})    
   })
+
+  // SET BUTTONS EVENT LISTENER
   $('.menu-btn').on('click', function() {
     const currentMenu = $('.collapse.show');
     currentMenu.removeClass('show');
   })
+  $('#continue-btn, #play-btn').on('click', hideMenu);
+  $('#play-btn').on('click', () => {
+    document.querySelector('#continue-btn').disabled = false;
+  })
+  $('#return-btn').on('click', hideGameScreen);
 }
+
+const showGameScreen = () => {
+  gameScreen.fadeIn(1000);
+}
+const hideGameScreen = () => {
+  gameScreen.fadeOut(1000, showMenu);
+}
+
+const hideMenu = () => {
+  menuWrapper.fadeOut(1000, showGameScreen);
+}
+const showMenu = () => {
+  menuWrapper.fadeIn(1000);
+}
+
+
+
+
+
 
 $(document).ready(function() {
   loadMenu();  
