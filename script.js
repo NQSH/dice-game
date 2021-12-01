bd61558ac5c1d79961953346bcd41960ecc629be
NQSH <thom.lecorre@gmail.com>
2021-12-01 15:54:59 +0100

    Update script.js
    > Modification temps d'animation

--- script.js
+++ script.js
@@ -5,11 +5,15 @@ const loadMenu = () => {
   setTimeout(() => {
     $('.menu-btn').first().click();
   }, 3000)
-  header.hide(3000);
-  menuWrapper.animate({
+  header
+  .delay(1500)
+  .hide(1500);
+  menuWrapper
+  .delay(1500)
+  .animate({
     'height': '150px',
     'opacity': '100%'
-  }, 3000, () => {
+  }, 1500, () => {
     menuWrapper.css('height', 'auto');
     $('.menu-name').animate({
       'opacity': '100%'
